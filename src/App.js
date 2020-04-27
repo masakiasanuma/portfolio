a533cd007bb206ad35b9564696f83ac281b2df48
Masaki <masakiasanuma@outlook.com>
2020-04-26 17:32:17 -0700

    Started messing around with some potential layouts for the website.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import Expansion from './components/expansions'
 
 const App = () => {
     return (
         <div>
-            <h1>Hello World!</h1>
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <h1>Masaki Asanuma</h1>
+                    <img src={require('./images/profile.jpg')} alt="Masaki Asanuma" class="center" />
+                </Grid>
+                <Grid item xs={12}>
+                    <Expansion />
+                </Grid>
+            </Grid>
+        
         </div>
     )
 }
---
+++ src/components/expansions.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Section from './section'
+
+const Expansion = () => {
+    return (
+        <div>
+            <Section title="About Me" body="Hey! I am a 2nd year Computer Science student at Georgia Institute of Technology." />
+            <Section title="Experiences" body="" />
+            <Section title="Projects" body="" />
+        </div>
+    )
+}
+
+export default Expansion
