243f7553906572cc2b22c827c88291f32ef8f570
Masaki <masakiasanuma@outlook.com>
2020-05-02 12:34:59 -0700

    Very basic layout is finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Container, Row, Col, Jumbotron, Image } from 'react-bootstrap' 
 
 const App = () => {
     return (
         <div>
-            Masaki Asanuma
+            <Container fluid>
+                <Row>
+                    <Col className="side" xs={3}>
+                        <Image className="profile" src={require("./images/profile.jpg")} fluid thumbnail />
+                        <div className="center">
+                            <h5>Email: masakiasanuma@outlook.com</h5>
+                            <h5>Phone: (310) 710-0790</h5>
+                        </div>
+                    </Col>
+                    <Col className="main" xs={9}>
+                        <Jumbotron>
+                            <h1 className="header">Hey!</h1>
+                        </Jumbotron>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
