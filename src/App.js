a0cb21c15abc601c08f7da78a0b084419aa03ee4
Masaki <masakiasanuma@outlook.com>
2020-05-02 13:24:50 -0700

    Finished sidebar layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import { Container, Row, Col, Jumbotron, Image } from 'react-bootstrap' 
+import { Container, Row, Col, Jumbotron, Image, Button } from 'react-bootstrap' 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const App = () => {
     return (
@@ -9,8 +12,10 @@ const App = () => {
                     <Col className="side" xs={3}>
                         <Image className="profile" src={require("./images/profile.jpg")} fluid thumbnail />
                         <div className="center">
-                            <h5>Email: masakiasanuma@outlook.com</h5>
-                            <h5>Phone: (310) 710-0790</h5>
+                            <h5><FontAwesomeIcon icon={faEnvelope} /> - masakiasanuma@outlook.com</h5>
+                            <h5><FontAwesomeIcon icon={faPhone} /> - (310) 710-0790</h5>
+                            <Button className="button" href="https://www.linkedin.com/in/masakiasanuma/" size="lg"><FontAwesomeIcon icon={faLinkedin} /></Button>
+                            <Button className="button" href="https://github.com/Masasasaki" size="lg"><FontAwesomeIcon icon={faGithub} /></Button>
                         </div>
                     </Col>
                     <Col className="main" xs={9}>
