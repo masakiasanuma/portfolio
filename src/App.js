5ccb60fdbf08a3547eb95618a588395e1b85005c
Masaki <masakiasanuma@outlook.com>
2020-05-02 11:34:16 -0700

    Let's start fresh...

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import React from 'react'
-import Grid from '@material-ui/core/Grid'
-import Expansion from './components/expansions'
 
 const App = () => {
     return (
         <div>
-            <Grid container spacing={3}>
-                <Grid item xs={12}>
-                    <h1>Masaki Asanuma</h1>
-                    <img src={require('./images/profile.jpg')} alt="Masaki Asanuma" class="center" />
-                </Grid>
-                <Grid item xs={12}>
-                    <Expansion />
-                </Grid>
-            </Grid>
-        
+            Masaki Asanuma
         </div>
     )
 }
