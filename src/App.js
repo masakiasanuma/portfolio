16b0d9a7069a5cd5a1218c2b3ecd9395332e0206
Masaki <masakiasanuma@outlook.com>
2020-05-02 21:37:39 -0700

    Temporary fix for the sticky

--- src/App.js
+++ src/App.js
@@ -2,20 +2,53 @@ import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap' 
 import Side from './components/side'
 import AboutMe from './components/aboutme'
+import Experience from './components/experience'
+import Skills from './components/skills'
+import Projects from './components/projects'
 
 const App = () => {
+    const containerStyle = {
+        backgroundColor: 'black'
+    }
+
     return (
         <div>
-            <Container fluid>
+            <Container style={containerStyle} fluid>
                 <Row>
                     <Col className="side" sm={12} md={6} lg={4}>
                         <Side />
                     </Col>
                     <Col className="main" sm={12} md={6} lg={8}>
-                        <AboutMe />
-                        <div>
-                            <h2 className="header">Experience</h2>
-                        </div>
+                        <Container fluid>
+                            <Row>
+                                <AboutMe />
+                            </Row>
+                            <Row className="section">
+                                <h2 className="header">Experience</h2>
+                                <hr className="separator" />
+                                <Experience />
+                            </Row>
+                            <Row className="section">
+                                <h2 className="header">Projects</h2>
+                                <hr className="separator" />
+                                <Projects />
+                            </Row>
+                            <Row className="section">
+                                <h2 className="header">Skills</h2>
+                                <hr className="separator" />
+                                <Skills />
+                            </Row>
+                            <Row className="section">
+                                <h2 className="header">Activities</h2>
+                                <hr className="separator" />
+                                <Skills />
+                            </Row>
+                            <Row className="section">
+                                <h2 className="header">Hobbies</h2>
+                                <hr className="separator" />
+                                <Skills />
+                            </Row>
+                        </Container>
                     </Col>
                 </Row>
             </Container>
---
+++ src/components/projects.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Projects = () => {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a ultrices ipsum. Donec varius in orci quis bibendum. Etiam at odio sapien. Fusce fermentum sed nisl nec fermentum. Aliquam egestas diam sit amet mi lacinia, in fermentum tortor vestibulum. Nullam id dui sem. Morbi egestas id nibh ac volutpat.
+
+Mauris eu dolor auctor, lacinia libero mattis, sodales sem. In orci augue, commodo at tempus eget, cursus quis lorem. Sed commodo mollis diam, a aliquam massa tempor at. Nam sodales turpis fringilla nulla mattis, ac cursus quam rutrum. Aenean ultrices cursus quam. Praesent maximus, urna eget rutrum condimentum, nibh mauris rhoncus est, sed dictum diam neque et augue. Phasellus vel ullamcorper nibh. Etiam tincidunt hendrerit posuere. Aliquam gravida leo eu orci convallis tempus. Sed tortor nisl, viverra eget accumsan id, dapibus faucibus nibh. Sed euismod at libero eu ornare.
+        </div>
+    )
+}
+
+export default Projects
