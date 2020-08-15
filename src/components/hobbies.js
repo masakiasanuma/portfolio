eb53671152fd46e058b7f3571775cf3ac0263994
Masaki <masakiasanuma@outlook.com>
2020-08-15 09:46:08 -0700

    Viewport changes

--- src/components/hobbies.js
+++ src/components/hobbies.js
@@ -7,12 +7,12 @@ const Hobbies = () => {
         <Container>
             <Row className="hobbyDesc">
                 <p>
-                    In my free time, you can find me playing the guitar, practicing latte art, or taking way too many naps.
-                    I also post fingerstyle covers of various Japanese songs on <a href="https://www.youtube.com/channel/UCth0pz1bQjWFVoX422pMypQ" target="_blank" rel="noopener noreferrer">Youtube</a> and <a href="https://www.instagram.com/masachanmusic/" target="_blank" rel="noopener noreferrer">Instagram</a>!
+                    In my free time, you can find me playing the guitar, practicing latte art, or browsing reddit and youtube.
+                    I also post fingerstyle guitar videos on <a href="https://www.youtube.com/channel/UCth0pz1bQjWFVoX422pMypQ" target="_blank" rel="noopener noreferrer">Youtube</a> and <a href="https://www.instagram.com/masachanmusic/" target="_blank" rel="noopener noreferrer">Instagram</a>!
                 </p>
             </Row>
             <Row className="video">
-                <ReactPlayer url="https://www.youtube.com/watch?v=bDJS5U82DhI" />
+                <ReactPlayer url="https://www.youtube.com/watch?v=VZLt256Uygo" />
             </Row>
         </Container>
     )
--- src/components/skills.js
+++ src/components/skills.js
@@ -11,13 +11,13 @@ const Skills = () => {
                         <i title="Java" class="skillIcon devicon-java-plain colored" />
                         <i title="Javascript" class="skillIcon devicon-javascript-plain colored" />
                         <i title="HTML5" class="skillIcon devicon-html5-plain colored" />
-                        <i title="C" class="skillIcon devicon-c-plain colored" />
+                        <i title="CSS3" class="skillIcon devicon-css3-plain colored" />
                         <i title="React.js" class="skillIcon devicon-react-original colored" />
                         <i title="Node.js" class="skillIcon devicon-nodejs-plain colored" />
                         <i title="Express.js" class="skillIcon devicon-express-original colored" />
                         <i title="MongoDB" class="skillIcon devicon-mongodb-plain colored" />
+                        <i title="MySQL" class="skillIcon devicon-mysql-plain colored" />
                         <i title="Git" class="skillIcon devicon-git-plain colored" />
-                        <i title="MySQL" class="skillIcon devicon-mysql-plain colored"></i>
                         <i title="Ubuntu" class="skillIcon devicon-ubuntu-plain colored" />
                         <i title="Windows" class="skillIcon devicon-windows8-original colored" />
                     </Col>
@@ -27,7 +27,7 @@ const Skills = () => {
                 <Row>
                     <Col xs={12} sm={4} md={3}><h4 className="skillLevel">Familiar: </h4></Col>
                     <Col xs={12} sm={8} md={9}>
-                        <i title="CSS3" class="skillIcon devicon-css3-plain colored" />
+                        <i title="C" class="skillIcon devicon-c-plain colored" />
                         <i title="Python" class="skillIcon devicon-python-plain colored" />
                         <i title="Bootstrap" class="skillIcon devicon-bootstrap-plain colored" />
                         <i title="jQuery" class="skillIcon devicon-jquery-plain colored" />
